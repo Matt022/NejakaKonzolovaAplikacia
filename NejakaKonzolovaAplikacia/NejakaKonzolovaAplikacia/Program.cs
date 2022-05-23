7e9f1c6ff221986be8bc3f057c983982e316a9e6
Matt022 <matusbenus02@gmail.com>
2022-05-23 23:29:22 +0200

    first commit

---
+++ NejakaKonzolovaAplikacia/NejakaKonzolovaAplikacia/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace JustSimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type your name: ");
+            string? name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
