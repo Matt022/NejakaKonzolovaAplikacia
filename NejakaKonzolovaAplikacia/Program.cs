b73bbf57bb05eae08856219adb23fbf48c575507
Matt022 <matusbenus02@gmail.com>
2022-05-23 23:25:40 +0200

    first commit

--- NejakaKonzolovaAplikacia/Program.cs
+++ NejakaKonzolovaAplikacia/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace JustSimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type your name: ");
+            string? name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
