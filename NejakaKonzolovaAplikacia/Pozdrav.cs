6671e21aa9ca6e39348e5b271d0abbb25d231391
Matt022 <matusbenus02@gmail.com>
2022-05-23 23:44:21 +0200

    new file added

--- NejakaKonzolovaAplikacia/Pozdrav.cs
+++ NejakaKonzolovaAplikacia/Pozdrav.cs
@@ -1,9 +1,12 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace NejakaKonzolovaAplikacia
 {
-    public class Pozdrav
+    internal class Pozdrav
     {
-
     }
 }
